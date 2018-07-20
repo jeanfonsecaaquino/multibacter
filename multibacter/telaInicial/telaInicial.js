0544be7fab047dcb1a1846cf466695dc49ca6fb7
Jean <jean@MacBook-Pro-de-Rafael.local>
2018-07-20 11:38:24 -0300

    Componentizando Cards

--- multibacter/telaInicial/telaInicial.js
+++ multibacter/telaInicial/telaInicial.js
@@ -10,48 +10,31 @@ import {
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeigth = Dimensions.get('window').height;
-let screenHeightHalf = screenHeigth / 2;
-let screenWidthHalf = screenWidth / 2;
+const screenHeightHalf = screenHeigth / 2;
+const screenWidthHalf = screenWidth / 2;
 const PERCENTUAL_HEIGTH = 36;
 const PERCENTUAL_WIDTH = 40;
-let screenCenterHeigh = (screenHeigth * PERCENTUAL_HEIGTH) / 100;
-let screenCenterWidth = (screenWidth * PERCENTUAL_WIDTH) / 100;
+const screenCenterHeigh = (screenHeigth * PERCENTUAL_HEIGTH) / 100;
+const screenCenterWidth = (screenWidth * PERCENTUAL_WIDTH) / 100;
+import Card from './card'
 
 
 export default class TelaInicial extends Component {
     render() {
         return (
-            <View style={{ flex: 1, flexDirection: 'column' }}>
-                <View style={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: 110
-                }}>
+            <View style={styles.viewColumn}>
+                <View style={styles.viewTextoLogo}>
                     <Image style={{ width: screenHeightHalf, height: 62, top: 10 }} source={require("../assets/logo_texto.png")} />
                 </View>
-                <Image source={require("../assets/logo.png")} style={{
-                    position: 'absolute',
-                    zIndex: 1,
-                    bottom: screenCenterHeigh,
-                    left: screenCenterWidth,
-                    width: 70, height: 70
-                }} />
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <View style={{ flex: 1, flexDirection: 'column' }}>
-                        <View style={styles.soja}>
-                            <Image style={{ width: 70, height: 70 }} source={require("../assets/icone_soja.png")} />
-                        </View>
-                        <View style={styles.algodao}>
-                            <Image style={{ width: 70, height: 70 }} source={require("../assets/icone_algodao.png")} />
-                        </View>
+                <Image source={require("../assets/logo.png")} style={styles.styleLogo} />
+                <View style={styles.viewRow}>
+                    <View style={styles.viewColumn}>
+                        <Card corFundo={"#f4ba41"} width={screenWidthHalf} height={screenHeightHalf} icone={"iconeSoja"} />
+                        <Card corFundo={"#3a281e"} width={screenWidthHalf} height={screenHeightHalf} icone={"iconeAlgodao"} />
                     </View>
-                    <View style={{ flex: 1, flexDirection: 'column' }}>
-                        <View style={styles.milho}>
-                            <Image style={{ width: 70, height: 70 }} source={require("../assets/icone_milho.png")} />
-                        </View>
-                        <View style={styles.cana}>
-                            <Image style={{ width: 70, height: 70 }} source={require("../assets/icone_cana.png")} />
-                        </View>
+                    <View style={styles.viewColumn}>
+                        <Card corFundo={"#9ead35"} width={screenWidthHalf} height={screenHeightHalf} icone={"iconeMilho"} />
+                        <Card corFundo={"#e76632"} width={screenWidthHalf} height={screenHeightHalf} icone={"iconeCana"} />
                     </View>
                 </View>
             </View >
@@ -60,41 +43,29 @@ export default class TelaInicial extends Component {
 }
 
 const styles = StyleSheet.create({
-    soja: {
-        backgroundColor: '#f4ba41',
-        width: screenWidthHalf,
-        height: screenHeightHalf,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    algodao: {
-        backgroundColor: '#3a281e',
-        width: screenWidthHalf,
-        height: screenHeightHalf,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    milho: {
-        backgroundColor: '#9ead35',
-        width: screenWidthHalf,
-        height: screenHeightHalf,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    cana: {
-        backgroundColor: '#e76632',
-        width: screenWidthHalf,
-        height: screenHeightHalf,
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
     logo: {
         resizeMode: "center",
         flex: 1
+    },
+    viewRow: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    viewColumn: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    styleLogo: {
+        position: 'absolute',
+        zIndex: 1,
+        bottom: screenCenterHeigh,
+        left: screenCenterWidth,
+        width: 70, height: 70
+    },
+    viewTextoLogo: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 110
     }
 });
 
